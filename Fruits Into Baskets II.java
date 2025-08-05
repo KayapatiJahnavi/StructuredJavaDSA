9f91db9c1fb091d08214c938a806d89a6383de3d
KayapatiJahnavi <kayapatijahnavi@gmail.com>
2025-08-05 10:22:11 +0530

    Create Fruits Into Baskets II.java

---
+++ Fruits Into Baskets II.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int numOfUnplacedFruits(int[] fruits, int[] baskets) {
+        int n = fruits.length;
+        boolean[] used = new boolean[baskets.length];  // track used baskets
+        int unplaced = 0;
+
+        for (int i = 0; i < n; i++) {
+            boolean placed = false;
+            for (int j = 0; j < baskets.length; j++) {
+                if (!used[j] && baskets[j] >= fruits[i]) {
+                    used[j] = true;
+                    placed = true;
+                    break;
+                }
+            }
+            if (!placed) {
+                unplaced++;
+            }
+        }
+
+        return unplaced;
+    }
+}
